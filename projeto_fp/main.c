6073c369a0473a63288ee6d6892c48c47681bb93
2211853 <92575572+2211853@users.noreply.github.com>
2021-12-22 17:11:41 +0000

    Update main.c

--- projeto_fp/main.c
+++ projeto_fp/main.c
@@ -5,3 +5,15 @@ void main()
 {
 
 }
+
+int confirmar_saida()
+{
+    int opcao;
+    do
+    {
+        printf("\n\nTem certeza que pretende sair:\n0 - Sim\n9 - nao\nOPCAO ->");
+        scanf("%d", &opcao);
+
+    }while(opcao != 0 && opcao != 9);
+    return opcao;
+}
