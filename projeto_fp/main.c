e5e4c5ef1ecdcd0a4d3076c0005a47663c5c10dd
2211853 <92575572+2211853@users.noreply.github.com>
2021-12-22 17:30:27 +0000

    Update main.c
    Funçao menu_opçoes

--- projeto_fp/main.c
+++ projeto_fp/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//denis
+int menu_opcoes(void);
+int confirmar_saida(void);
+
+
 void main()
 {
 
 }
 
+//-------------serve para confirmar a saida do utilizador-----------------
 int confirmar_saida()
 {
     int opcao;
@@ -16,5 +20,19 @@ int confirmar_saida()
         scanf("%d", &opcao);
 
     }while(opcao != 0 && opcao != 9);
+    return opcao;
+}
+
+//-------------serve para exibir e receber a opcao do menu-------------------
+int menu_opcoes()
+{
+    int opcao;
+    do
+    {
+        system("cls");
+        printf("\n\nEscolha a sua Opcao:\n1 - Registar escola\n2 - Registar utilizador\n3 - Registar transação\n4 - Consultar escolas\n5 - Consultar utilizadores\n6 - Consultar transaçõesv\n7 - Verificar estatisticas\n0 - sair\n\tOPCAO ->");
+        scanf("%d", &opcao);
+
+    }while(opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5 && opcao != 6 && opcao != 7 && opcao != 0);
     return opcao;
 }
